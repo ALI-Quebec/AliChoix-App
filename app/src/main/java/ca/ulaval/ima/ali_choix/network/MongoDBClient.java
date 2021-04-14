9f93af1cdf63eed292c8cea59940e2f351d11415
JCBoorgo <jcbourgault2008@yahoo.ca>
2021-04-13 23:49:27 -0400

    Fully fledged find function Should have everything it needs

--- app/src/main/java/ca/ulaval/ima/ali_choix/network/MongoDBClient.java
+++ app/src/main/java/ca/ulaval/ima/ali_choix/network/MongoDBClient.java
@@ -1,6 +1,7 @@
 package ca.ulaval.ima.ali_choix.network;
 
 import com.mongodb.ConnectionString;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -8,8 +9,10 @@ import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
 import ca.ulaval.ima.ali_choix.BuildConfig;
+import ca.ulaval.ima.ali_choix.network.exceptions.NotFoundException;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.inc;
 
 public class MongoDBClient {
     private final ConnectionString MONGODB_CONNECTION = new ConnectionString(BuildConfig.MONGODB_CONNECTION);
@@ -17,22 +20,39 @@ public class MongoDBClient {
     private final String PRODUCTS_COLLECTION_NAME = "products";
     private final String HISTORY_COLLECTION_NAME = "history";
     private final String MISSING_PRODUCTS_COLLECTION_NAME = "missingproducts";
+    private final String SCAN_COUNT_FIELD_NAME = "scanCount";
 
     public String findProduct(String productID) {
         MongoClient mongoClient = MongoClients.create(MONGODB_CONNECTION);
-        MongoCollection<Document> collection = mongoClient.getDatabase(DATABASE_NAME).getCollection(PRODUCTS_COLLECTION_NAME);
-        //TODO what if not found?
-        String productJson = collection.find(eq("_id", productID)).first().toJson();
+        MongoCollection<Document> productsCollection = mongoClient.getDatabase(DATABASE_NAME).getCollection(PRODUCTS_COLLECTION_NAME);
+        FindIterable<Document> productSearch = productsCollection.find(eq("_id", productID));
+        if (productSearch == null) {
+            addToMissingProducts(mongoClient, productID);
+            // This throws an exception so it never executes the rest
+        }
+        incrementHistoryCounter(mongoClient, productID);
+        String productJson = productSearch.first().toJson();
         mongoClient.close();
         return productJson;
     }
 
-    public String findHistoryOfProduct(String productID) {
-        MongoClient mongoClient = MongoClients.create(MONGODB_CONNECTION);
-        MongoCollection<Document> collection = mongoClient.getDatabase(DATABASE_NAME).getCollection(HISTORY_COLLECTION_NAME);
-        //TODO what if not found?
-        String productJson = collection.find(eq("_id", productID)).first().toJson();
+    private void addToMissingProducts(MongoClient mongoClient, String productID) {
+        MongoCollection<Document> collection = mongoClient.getDatabase(DATABASE_NAME).getCollection(MISSING_PRODUCTS_COLLECTION_NAME);
+        FindIterable<Document> search = collection.find(eq("_id", productID));
+        // Only create the object if one doesn't exist yet
+        if (search == null) {
+            collection.insertOne(new Document("_id", productID));
+        }
         mongoClient.close();
-        return productJson;
+        throw new NotFoundException();
+    }
+
+    private void incrementHistoryCounter(MongoClient mongoClient, String productID) {
+        MongoCollection<Document> collection = mongoClient.getDatabase(DATABASE_NAME).getCollection(HISTORY_COLLECTION_NAME);
+        Document productHistory = collection.findOneAndUpdate(eq("_id", productID), inc(SCAN_COUNT_FIELD_NAME, 1));
+        if (productHistory == null) {
+            // Create one if it doesn't exist yet
+            collection.insertOne(new Document("_id", productID).append(SCAN_COUNT_FIELD_NAME, 1));
+        }
     }
 }
---
+++ app/src/main/java/ca/ulaval/ima/ali_choix/network/exceptions/NotFoundException.java
@@ -0,0 +1,24 @@
+package ca.ulaval.ima.ali_choix.network.exceptions;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import ca.ulaval.ima.ali_choix.domain.ErrorCode;
+import ca.ulaval.ima.ali_choix.domain.exceptions.ApplicationException;
+
+public class NotFoundException extends ApplicationException {
+    private static final String ERROR = "Product cannot be found";
+    private static final String DESCRIPTION = "Cannot get searched product because it isn't in the database";
+    private static final ErrorCode CODE = ErrorCode.NOT_FOUND;
+
+    public NotFoundException() {
+        super(ERROR, DESCRIPTION, CODE);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public String getDescription() {
+        return String.format(DESCRIPTION);
+    }
+}
